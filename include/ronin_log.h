ee73a8323ee3fe9560421ce06f09d5bcf4891b48
intellibits28 <intellibits@gmail.com>
2026-04-15 14:54:48 +0630

    Fix: Define LOGW macro in ronin_log.h [v3.9.5-STABLE]

--- include/ronin_log.h
+++ include/ronin_log.h
@@ -3,9 +3,11 @@
 #ifdef ANDROID
 #include <android/log.h>
 #define LOGI(tag, ...) __android_log_print(ANDROID_LOG_INFO, tag, __VA_ARGS__)
+#define LOGW(tag, ...) __android_log_print(ANDROID_LOG_WARN, tag, __VA_ARGS__)
 #define LOGE(tag, ...) __android_log_print(ANDROID_LOG_ERROR, tag, __VA_ARGS__)
 #else
 #include <cstdio>
 #define LOGI(tag, ...) printf("[%s] INFO: ", tag); printf(__VA_ARGS__); printf("\n")
+#define LOGW(tag, ...) printf("[%s] WARN: ", tag); printf(__VA_ARGS__); printf("\n")
 #define LOGE(tag, ...) fprintf(stderr, "[%s] ERROR: ", tag); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n")
 #endif
